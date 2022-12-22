d60ed191426c86094d3781536bd129a49af7b844
lucianRosuAtc <lucian.rosu.atc@gmail.com>
2022-12-22 16:03:23 +0000

    update

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs'
+import { MdVideoCameraFront } from 'react-icons/md'
 import { meal } from '../../constants'
 import './Intro.css'
 
 const Intro = () => {
+  const[playVideo, setPlayVideo] = useState(false)
   const vidRef = React.useRef()
+
+  const handleVideo = ()=>{
+    setPlayVideo((prevPlayVideo)=>!prevPlayVideo)
+
+    if(playVideo){
+      vidRef.current.pause()
+    } else{
+      vidRef.current.play()
+    }
+  }
+
   return(
     <div className='app__video'>
       <video
@@ -15,6 +28,18 @@ const Intro = () => {
       controls={false}
       muted
       />
+
+      <div className='app__video-overlay flex__center'>
+        <div 
+         className='app__video-overlay_circle flex__center'
+         onClick={handleVideo}
+        > 
+          {playVideo
+            ?<BsPauseFill color="#fff" fontSize={30} />
+            :<BsFillPlayFill color="#fff" fontSize={30} />
+          }
+        </div>
+      </div>
     </div>
   )
 }
